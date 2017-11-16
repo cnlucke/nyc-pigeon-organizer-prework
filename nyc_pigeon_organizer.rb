ee71beca440e61f600e66f22993cb6ad1b7db654
Cristy Lucke <cristy.lucke@gmail.com>
2017-11-16 17:44:05 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -33,7 +33,7 @@ def nyc_pigeon_organizer(data)
         puts "Pigeon is #{pigeon} and trait_holder is #{trait_holder} and trait_hash is #{trait_hash}"
         if pigeon_list.has_key?(pigeon)
           if pigeon_list[pigeon].has_key?(trait_holder)
-            pigeon_list[pigeon][trait_holder] + trait_hash[trait_holder]
+            (pigeon_list[pigeon][trait_holder]).push(*trait_hash[trait_holder])
           else
             pigeon_list[pigeon][trait_holder] = []
             pigeon_list[pigeon][trait_holder] = trait_hash[trait_holder]
